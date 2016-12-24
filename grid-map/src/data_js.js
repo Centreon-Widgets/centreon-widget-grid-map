360de5f6747eec7cdb923eb7318646886dfd3985
Erkan <rk4an@users.noreply.github.com>
2016-12-24 09:03:15 +0100

    Update data_js.js

--- grid-map/src/data_js.js
+++ grid-map/src/data_js.js
@@ -1,27 +1,35 @@
 var timeout;
 
-jQuery(function() {
-    console.log("jQuery function");
-    loadTop10();
-});
-
-function loadTop10() {
-    jQuery.ajax("./index.php", {
+function loadgridMap() {
+   jQuery.ajax("./index.php?widgetId=" + widgetId, {
         success : function(htmlData) {
-            jQuery("#infoAjax").html("");
-            jQuery("#infoAjax").html(htmlData);
-            var h = document.getElementById("Grid-map").scrollHeight + 10;
-            if (h){
-                parent.iResize(window.name, h);
-            } else { 
-                parent.iResize(window.name, 200);
-            }
+            jQuery("#Grid-map").html("");
+            jQuery("#Grid-map").html(htmlData);
         }
     });
-    if (autoRefresh) {
+    resizegridMap();
+}
+
+function resizegridMap() {
+            var h = document.getElementById("map-icon");
+            if (h){
+                parent.iResize(window.name, h.scrollHeight + 10);
+            } else {
+               parent.iResize(window.name, 200);
+            }
+}
+
+function refreshgridMap() {
+  //location.reload();
+  loadgridMap();
+}
+
+jQuery(function() {
+    if (autoRefresh > 0) {
         if (timeout) {
             clearTimeout(timeout);
         }
-        timeout = setTimeout(loadTop10, (30 * 1000));
+        timeout = setTimeout(refreshgridMap, (autoRefresh * 1000));
     }
-}
+    resizegridMap();
+});
