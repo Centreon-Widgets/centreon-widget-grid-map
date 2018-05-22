081bc2da839eda317b1d79724b6d3baae759eaf8
v.radev <v.radev@live.com>
2018-05-22 10:09:56 +0300

    fix(global): remove manual smarty loading

--- grid-map/src/table.php
+++ grid-map/src/table.php
@@ -47,9 +47,6 @@ require_once $centreon_path . 'www/class/centreonUtils.class.php';
 require_once $centreon_path . 'www/class/centreonACL.class.php';
 require_once $centreon_path . 'www/class/centreonHost.class.php';
 
-/* load smarty Class */
-require_once $centreon_path . 'GPL_LIB/Smarty/libs/Smarty.class.php';
-
 CentreonSession::start(1);
 
 if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId'])) {
